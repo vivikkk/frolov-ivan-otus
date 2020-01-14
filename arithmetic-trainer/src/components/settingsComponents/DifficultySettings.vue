6619c17191e82e32833d80c302c6346bb1ff55f3
Ivan Frolov <ivanushka.frolov@gmail.com>
2020-01-14 18:44:41 +0300

    base welcome screen

---
+++ arithmetic-trainer/src/components/settingsComponents/DifficultySettings.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-row>
+        <v-col cols="12">
+          <h3>Сложность:
+            <span v-text="difficulty"></span>
+          </h3>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="6">
+          <v-slider
+            v-model="difficulty"
+            min="1"
+            max="4"
+            tick-size="4"
+            :tick-labels="seasons"
+            :color="getDifficultColor"
+            ticks="always"
+          >
+          </v-slider>
+        </v-col>
+      </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DifficultySettings',
+
+  data: () => ({
+    difficulty: 2,
+    colors: [
+      '#EF9A9A',
+      '#EF5350',
+      '#D32F2F',
+      '#B71C1C'
+    ],
+    seasons: [
+      'Изян',
+      'Просто',
+      'Норм',
+      'Сложно'
+    ]
+  }),
+
+  computed: {
+    getDifficultColor () {
+      return this.colors[this.difficulty - 1]
+    }
+  }
+}
+</script>
---
+++ arithmetic-trainer/src/views/Welcome.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <Splash/>
+
+    <v-container class="blue-grey lighten-5 elevation-5">
+      <v-row>
+        <v-col cols="12">
+          <h2>Настройки</h2>
+        </v-col>
+      </v-row>
+
+      <DurationSettings/>
+      <v-divider></v-divider>
+
+      <DifficultySettings/>
+      <v-divider></v-divider>
+      <Computations/>
+    </v-container>
+    <v-btn class="mt-8 mb-6" block color="primary">Play!</v-btn>
+  </v-container>
+</template>
+
+<script>
+import Splash from '../components/Splash'
+import DurationSettings from '../components/settingsComponents/DurationSettings'
+import DifficultySettings from '../components/settingsComponents/DifficultySettings'
+import Computations from '../components/settingsComponents/Computations'
+
+export default {
+  name: 'Welcome',
+
+  components: {
+    Splash,
+    DurationSettings,
+    DifficultySettings,
+    Computations
+  }
+}
+</script>
